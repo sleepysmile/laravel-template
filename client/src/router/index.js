a59241696a35bba66cd9c0df28aedfbb86f56948
Roman Ostrowskij <ostrovskiy@ruport.ru>
2026-01-27 18:51:48 +0300

    remove unused

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,40 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import ChatList from '../views/ChatList.vue'
-import ChatDetail from '../views/ChatDetail.vue'
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: LoginView,
+      name: 'home',
+      component: Home,
     },
-    {
-      path: '/chats',
-      name: 'chat-list',
-      component: ChatList,
-    },
-    {
-      path: '/chats/:id',
-      name: 'chat-detail',
-      component: ChatDetail,
-    }
   ],
-})
-
-router.beforeEach((to) => {
-  const token = localStorage.getItem('auth_token')
-
-  if (!token && to.name !== 'login') {
-    return { name: 'login', query: { redirect: to.fullPath } }
-  }
-
-  if (token && to.name === 'login') {
-    const redirectPath = typeof to.query.redirect === 'string' ? to.query.redirect : '/chats'
-    return { path: redirectPath }
-  }
 })
 
 export default router
